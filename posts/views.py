c0d6145f613eb836be3aae72beb1796dd4232f40
Benjamin Masor <benjaminmasor@gmail.com>
2022-08-10 11:52:41 +0900

    added: feature/post_crud

---
+++ posts/views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Post
+from .serializers import PostSerializer
+
+
+class PostModelViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
